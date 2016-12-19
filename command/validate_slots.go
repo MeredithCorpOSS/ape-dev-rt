bd46603b9ec855cdbf4f9d78fd6eaeca109868fb
Sarah Connor <sarah.connor@timeinc.com>
2016-12-19 15:31:24 +0000

    added validate-slots command
    WIP
    
    WIP
    
    WIP

--- command/validate_slots.go
+++ command/validate_slots.go
@@ -2,7 +2,10 @@ package command
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/TimeInc/ape-dev-rt/aws"
 	"github.com/TimeInc/ape-dev-rt/commons"
@@ -30,6 +33,22 @@ func ValidateSlots(c *commons.Context) error {
 		return fmt.Errorf("%q does not exist", cfgPath)
 	}
 
+	files, err := ioutil.ReadDir(cfgPath)
+	if err != nil {
+		return err
+	}
+
+	if len(files) < 1 {
+		return fmt.Errorf("Current working directory %s is empty", cfgPath)
+	}
+
+	directories := make([]string, 0)
+	for _, file := range files {
+		if file.Mode().IsDir() && !strings.HasPrefix(file.Name(), ".") {
+			directories = append(directories, file.Name())
+		}
+	}
+
 	appData, exists, err := BeginApplicationOperation(c.String("env"), c.String("app"), ds)
 	if err != nil {
 		return err
@@ -42,28 +61,36 @@ func ValidateSlots(c *commons.Context) error {
 		return deprecatedGitError()
 	}
 
+	namespace := user.AccountID
+	if c.String("namespace") != "default" {
+		namespace = c.String("namespace")
+	}
+
 	filesToCleanup := make([]string, 0)
 	templateVars := VersionTemplateVars{
-		AwsAccountId: user.AccountID,
+		AwsAccountId: namespace,
 		Environment:  c.String("env"),
 		AppName:      c.String("app"),
 	}
-	filesToCleanup, err = commons.ProcessTemplates(cfgPath, "tpl", templateVars)
-	if err != nil {
-		return err
+
+	for _, directory := range directories {
+		rootDir := path.Join(cfgPath, directory)
+		filesToCleanup, err = commons.ProcessTemplates(rootDir, "tpl", templateVars)
+		if err != nil {
+			return err
+		}
+
+		vo, err := terraform.Validate(rootDir)
+		if err != nil {
+			return err
+		}
+		if vo.ExitCode != 0 {
+			return fmt.Errorf("validate operation failed (exit code %d). Stderr:\n%s",
+				vo.ExitCode, vo.Stderr)
+		}
 	}
 
-	vo, err := terraform.Validate(cfgPath)
-	if err != nil {
-		return err
-	}
-
-	if vo.ExitCode != 0 {
-		return fmt.Errorf("validate operation failed (exit code %d). Stderr:\n%s",
-			vo.ExitCode, vo.Stderr)
-	}
-
-	fmt.Printf("(/◕ヮ◕)/\nTerraform code for app '%s' in environment '%s' is valid.\n", c.String("app"), c.String("env"))
+	fmt.Printf("\\(◕ヮ◕)/\nTerraform code for app '%s' in environment '%s' is valid.\n\n", c.String("app"), c.String("env"))
 
 	return cleanupFilePaths(filesToCleanup)
 }
