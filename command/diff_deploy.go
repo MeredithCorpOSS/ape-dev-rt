659990e48d1e324330caab8549008d661f4df8ec
Zac Shepherd <zac.shepherd@timeinc.com>
2017-11-08 13:54:24 -0500

    added commands to run terraform plan on infra and version levels: diff-infra diff-deploy

---
+++ command/diff_deploy.go
@@ -0,0 +1,152 @@
+package command
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/TimeInc/ape-dev-rt/aws"
+	"github.com/TimeInc/ape-dev-rt/commons"
+	"github.com/TimeInc/ape-dev-rt/deploymentstate"
+	"github.com/TimeInc/ape-dev-rt/hcl"
+	"github.com/TimeInc/ape-dev-rt/terraform"
+)
+
+func DiffDeploy(c *commons.Context) error {
+	user, ok := c.CliContext.App.Metadata["user"].(*aws.User)
+	if !ok {
+		return fmt.Errorf("Unable to find AWS User in metadata")
+	}
+
+	ds, ok := c.CliContext.App.Metadata["ds"].(*deploymentstate.DeploymentState)
+	if !ok {
+		return fmt.Errorf("Unable to find Deployment State in metadata")
+	}
+
+	rs, ok := c.CliContext.App.Metadata["remote_state"].(*hcl.RemoteState)
+	if !ok {
+		return fmt.Errorf("Unable to find Remote State in metadata")
+	}
+
+	cfgPath, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	appData, exists, err := BeginApplicationOperation(c.String("env"), c.String("app"), ds)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return nil
+	}
+
+	slotId := c.String("slot-id")
+	slotPrefix := c.String("slot-prefix")
+	if slotId == "" && slotPrefix == "" {
+		return fmt.Errorf("'slot-id' or 'slot-prefix' is required parameter for %q (migrated app)", c.String("app"))
+	}
+	if slotId != "" && slotPrefix != "" {
+		return errors.New("You can specify either 'slot-id' or 'slot-prefix', not both.")
+	}
+
+	if c.CliContext.NArg() < 1 {
+		return fmt.Errorf("You need to supply a path to Terraform configs of %q.", slotId)
+	}
+
+	if slotPrefix != "" {
+		counter, prefixExists, err := ds.GetSlotCounter(slotPrefix, appData)
+		if err != nil {
+			return err
+		}
+		if !prefixExists {
+			return fmt.Errorf("Slot prefix %s does not exist", slotPrefix)
+		}
+
+		oldSlotId := fmt.Sprintf("%s%d", slotPrefix, counter)
+		counter, appData, err = ds.IncrementSlotCounter(slotPrefix, appData)
+		if err != nil {
+			return err
+		}
+		slotId = fmt.Sprintf("%s%d", slotPrefix, counter)
+		fmt.Printf("Last slot ID is %s, preparing deploy into %s\n", oldSlotId, colour.boldWhite(slotId))
+	}
+
+	rootDir := path.Join(cfgPath, c.CliContext.Args().First())
+	if rootDir == cfgPath {
+		return fmt.Errorf("Terraform configs for a slot have to be in a separate dir, not in %q!", cfgPath)
+	}
+	_, err = os.Stat(rootDir)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("%q does not exist", rootDir)
+	}
+
+	namespace := user.AccountID
+	if c.String("namespace") != "default" {
+		namespace = c.String("namespace")
+	}
+
+	filesToCleanup := make([]string, 0)
+	templateVars := VersionTemplateVars{
+		AwsAccountId: namespace,
+		Environment:  c.String("env"),
+		AppName:      c.String("app"),
+	}
+	filesToCleanup, err = commons.ProcessTemplates(rootDir, "tpl", templateVars)
+	if err != nil {
+		return err
+	}
+
+	vars := c.StringSlice("var")
+	tfVariables := make(map[string]string, 0)
+	for _, v := range vars {
+		parts := strings.Split(v, "=")
+		key, value := parts[0], parts[1]
+		tfVariables[key] = value
+	}
+
+	tfVariables["app_name"] = c.String("app")
+	tfVariables["app_version"] = slotId
+	tfVariables["environment"] = c.String("env")
+
+	remoteState, err := terraform.GetRemoteStateForSlotId(&terraform.RemoteState{
+		Backend: rs.Backend,
+		Config:  rs.Config,
+	}, user.AccountID, c.String("app"), slotId)
+	if err != nil {
+		return err
+	}
+
+	planStartTime := time.Now().UTC()
+	planFilePath := path.Join(rootDir, slotId+"-planfile")
+	filesToCleanup = append(filesToCleanup, path.Join(rootDir, ".terraform"))
+	filesToCleanup = append(filesToCleanup, planFilePath)
+	out, err := terraform.FreshPlan(&terraform.FreshPlanInput{
+		RemoteState:  remoteState,
+		RootPath:     rootDir,
+		PlanFilePath: planFilePath,
+		Variables:    tfVariables,
+		Refresh:      true,
+		Target:       c.String("target"),
+		Destroy:      false,
+		XLegacy:      c.Bool("x"),
+	})
+	planFinishTime := time.Now().UTC()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[DEBUG] Plan started %s, finished %s",
+		planStartTime.String(), planFinishTime.String())
+
+	if out.ExitCode != 0 {
+		return fmt.Errorf("Planning failed (exit code %d). Stderr:\n%s",
+			out.ExitCode, out.Stderr)
+	}
+
+	return cleanupFilePaths(filesToCleanup)
+}
---
+++ command/diff_infra.go
@@ -0,0 +1,94 @@
+package command
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/TimeInc/ape-dev-rt/aws"
+	"github.com/TimeInc/ape-dev-rt/commons"
+	"github.com/TimeInc/ape-dev-rt/hcl"
+	"github.com/TimeInc/ape-dev-rt/terraform"
+)
+
+func DiffInfra(c *commons.Context) error {
+	user, ok := c.CliContext.App.Metadata["user"].(*aws.User)
+	if !ok {
+		return fmt.Errorf("Unable to find AWS User in metadata")
+	}
+
+	rs, ok := c.CliContext.App.Metadata["remote_state"].(*hcl.RemoteState)
+	if !ok {
+		return fmt.Errorf("Unable to find Remote State in metadata")
+	}
+
+	cfgPath, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	namespace := user.AccountID
+	if c.String("namespace") != "default" {
+		namespace = c.String("namespace")
+	}
+
+	rootDir := cfgPath
+	filesToCleanup := make([]string, 0)
+	templateVars := ApplicationTemplateVars{
+		AwsAccountId: namespace,
+		AppName:      c.String("app"),
+		Environment:  c.String("env"),
+	}
+	filesToCleanup, err = commons.ProcessTemplates(rootDir, "tpl", templateVars)
+	if err != nil {
+		return err
+	}
+
+	remoteState, err := terraform.GetRemoteStateForApp(&terraform.RemoteState{
+		Backend: rs.Backend,
+		Config:  rs.Config,
+	}, namespace, c.String("app"))
+	if err != nil {
+		return err
+	}
+
+	vars := c.StringSlice("var")
+	tfVariables := make(map[string]string, 0)
+	for _, v := range vars {
+		parts := strings.Split(v, "=")
+		key, value := parts[0], parts[1]
+		tfVariables[key] = value
+	}
+
+	tfVariables["app_name"] = c.String("app")
+	tfVariables["environment"] = c.String("env")
+	planStartTime := time.Now().UTC()
+	planFilePath := path.Join(rootDir, "planfile")
+	filesToCleanup = append(filesToCleanup, path.Join(rootDir, ".terraform"))
+	filesToCleanup = append(filesToCleanup, planFilePath)
+	planOut, err := terraform.FreshPlan(&terraform.FreshPlanInput{
+		RemoteState:  remoteState,
+		RootPath:     rootDir,
+		PlanFilePath: planFilePath,
+		Variables:    tfVariables,
+		Refresh:      true,
+		Target:       c.String("target"),
+		Destroy:      false,
+		XLegacy:      c.Bool("x"),
+	})
+	planFinishTime := time.Now().UTC()
+	if err != nil {
+		return err
+	}
+	log.Printf("[DEBUG] Plan started %s, finished %s",
+		planStartTime.String(), planFinishTime.String())
+
+	if planOut.ExitCode != 0 {
+		return fmt.Errorf("Plan failed (exit code %d). Stderr:\n", planOut.ExitCode, planOut.Stderr)
+	}
+
+	return cleanupFilePaths(filesToCleanup)
+}
