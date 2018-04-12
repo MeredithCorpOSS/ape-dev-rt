b9d126d5398cf68eabc094c899c6401a4166e2b2
Zac Shepherd <zac.shepherd@timeinc.com>
2018-04-12 06:24:45 -0400

    moved panicWrap to rt/main so that it's run as early as possible in execution; created realMain and wrappedMain to make this change simpler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,11 +12,73 @@ import (
 	"github.com/mitchellh/go-homedir"
 	"github.com/ttacon/chalk"
 	"github.com/urfave/cli"
+	"github.com/hashicorp/terraform/helper/logging"
+	"github.com/mitchellh/panicwrap"
+	"syscall"
 )
 
 var errorStyle = chalk.Red.NewStyle().WithTextStyle(chalk.Bold).Style
 
 func main() {
+	os.Exit(realMain())
+}
+
+func realMain() int {
+	var wrapConfig panicwrap.WrapConfig
+	if !panicwrap.Wrapped(&wrapConfig) {
+		// Determine where logs should go in general (requested by the user)
+		logWriter, err := logging.LogOutput()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't setup log output: %s", err)
+			return 1
+		}
+
+		// We always send logs to a temporary file that we use in case
+		// there is a panic. Otherwise, we delete it.
+		logTempFile, err := ioutil.TempFile("", "terraform-log")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't setup logging tempfile: %s", err)
+			return 1
+		}
+		defer os.Remove(logTempFile.Name())
+		defer logTempFile.Close()
+
+		// Setup the prefixed readers that send data properly to
+		// stdout/stderr.
+		doneCh := make(chan struct{})
+		outR, outW := io.Pipe()
+		go copyOutput(outR, doneCh)
+
+		// Create the configuration for panicwrap and wrap our executable
+		wrapConfig.Handler = panicHandler(logTempFile)
+		wrapConfig.Writer = io.MultiWriter(logTempFile, logWriter)
+		wrapConfig.Stdout = outW
+		wrapConfig.IgnoreSignals = []os.Signal{os.Interrupt}
+		wrapConfig.ForwardSignals = []os.Signal{syscall.SIGTERM}
+		exitStatus, err := panicwrap.Wrap(&wrapConfig)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't start Terraform: %s", err)
+			return 1
+		}
+		// If >= 0, we're the parent, so just exit
+		if exitStatus >= 0 {
+			// Close the stdout writer so that our copy process can finish
+			outW.Close()
+
+			// Wait for the output copying to finish
+			<-doneCh
+
+			return exitStatus
+		}
+
+		// We're the child, so just close the tempfile we made in order to
+		// save file handles since the tempfile is only used by the parent.
+		logTempFile.Close()
+	}
+	return wrappedMain()
+}
+
+func wrappedMain() int{
 	app := cli.NewApp()
 	app.Name = "release tool"
 	app.Usage = "For amazing releases"
@@ -54,8 +117,10 @@ func main() {
 	if err != nil {
 		// Since logging setup can fail too, we just use stderr
 		fmt.Fprintln(os.Stderr, errorStyle("[ERROR] "+err.Error()))
-		os.Exit(1)
+		return 1
 	}
+
+	return 0
 }
 
 func createLogFile() (*os.File, error) {
--- terraform/terraform.go
+++ terraform/terraform.go
@@ -11,13 +11,10 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"syscall"
 
 	"github.com/TimeInc/ape-dev-rt/ui"
 	"github.com/hashicorp/terraform/command"
-	"github.com/hashicorp/terraform/helper/logging"
 	m_cli "github.com/mitchellh/cli"
-	"github.com/mitchellh/panicwrap"
 )
 
 const AppName = "app"
@@ -390,40 +387,6 @@ func Validate(rootpath string) (*CmdOutput, error) {
 }
 
 func Cmd(cmdName string, args []string, basePath string, stdoutW, stderrW io.Writer) (*CmdOutput, error) {
-	var wrapConfig panicwrap.WrapConfig
-
-	// Determine where logs should go in general (requested by the user)
-	logWriter, err := logging.LogOutput()
-	if err != nil {
-		return nil, fmt.Errorf("Couldn't setup log output: %s", err)
-	}
-
-	// We always send logs to a temporary file that we use in case
-	// there is a panic. Otherwise, we delete it.
-	logTempFile, err := ioutil.TempFile("", "terraform-log")
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(logTempFile.Name())
-	defer logTempFile.Close()
-
-	// Setup the prefixed readers that send data properly to
-	// stdout/stderr.
-	doneCh := make(chan struct{})
-	outR, outW := io.Pipe()
-	go copyOutput(outR, doneCh)
-
-	// Create the configuration for panicwrap and wrap our executable
-	wrapConfig.Handler = panicHandler(logTempFile)
-	wrapConfig.Writer = io.MultiWriter(logTempFile, logWriter)
-	wrapConfig.Stdout = outW
-	wrapConfig.IgnoreSignals = []os.Signal{os.Interrupt}
-	wrapConfig.ForwardSignals = []os.Signal{syscall.SIGTERM}
-	_, err = panicwrap.Wrap(&wrapConfig)
-	if err != nil {
-		return nil, err
-	}
-
 	workDir, err := os.Getwd()
 	if err != nil {
 		return nil, err
@@ -478,7 +441,6 @@ func Cmd(cmdName string, args []string, basePath string, stdoutW, stderrW io.Wri
 	if !ok {
 		return nil, fmt.Errorf("Unknown Terraform command: %s", cmdName)
 	}
-
 
 	log.Printf("[DEBUG] Executing: terraform %s %q in path %s", cmdName, args, basePath)
 	exitCode := cmd.Run(args)
